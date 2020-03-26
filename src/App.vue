3f7f9d1e8e7f066379021d2f62b2529f4c525763
mk36 <mikeking36@hotmail.co.uk>
2020-03-26 22:08:03 +0000

    test commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<style lang="scss">
+
+</style>
+
+<template>
+  <div id="app">
+   <test-comp></test-comp>
+    <p>test</p>
+      <div class="home">
+          <p>hello</p>
+          <div class="columns">
+              <div class="column">1</div>
+              <div class="column">2</div>
+              <div class="column">3</div>
+              <div class="column">4</div>
+              <div class="column">5</div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import "./styles/styles.scss";
+import TestComp from './components/test-comp/test-comp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TestComp
+  }
+}
+</script>
